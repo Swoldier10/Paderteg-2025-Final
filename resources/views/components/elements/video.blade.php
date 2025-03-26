3d26f1e05ea933891416794f26614f09f1af596e
raulinocentiuneculai <neculai.inocentiu@gmail.com>
2025-03-26 12:52:14 +0200

    changes website - 26.03

--- resources/views/components/elements/video.blade.php
+++ resources/views/components/elements/video.blade.php
@@ -1,4 +1,4 @@
 <video {{ $attributes->merge(['class' => '']) }} loop muted autoplay>
-    <source src="{{ \Illuminate\Support\Facades\Storage::disk('s3')->url($src) }}" type="video/mp4">
+    <source src="{{ $src }}" type="video/mp4">
 </video>
 
